4d7e87b8f6ef3a528773ce1c1f775b977225d380
wTheRockb <forrestwbutler@gmail.com>
2020-11-24 18:03:32 -0500

    add

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Welcome to Woody's Wacky World of constituents!
+Basic CRA to display some constituent data, originally in CSV format.
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import * as React from 'react';
+import Navbar from './components/navbar';
+import './style.scss'
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="root">
+        <Navbar />
+      </div>
+  )
 }
 
 export default App;
+
+
+// Prompt: Assume that you have been given a CSV file of constituent data. 
+// The header row is Email,FirstName,LastName,ZipCode. Create a web page that reads the data from a data store of your 
+// choice and displays it using a simple React application.
